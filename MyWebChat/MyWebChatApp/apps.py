b419774a7b49b752ae246092d925b8f3098a616c
Sergey.Matsak <matsak@Gavrischuks-Mac-mini.local>
2016-08-25 10:36:24 +0300

    delete project

--- MyWebChat/MyWebChatApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MywebchatappConfig(AppConfig):
-    name = 'MyWebChatApp'
--- MyWebChat/MyWebChatApp/login.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import redirect
-
-def login_in(request):
-    login = request.POST.get('inputLogin')
-    password = request.POST.get('inputPassword')
-    return redirect("/login")
